b35ea3745a46f6110619cfbaf342bd35701a53c9
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-03-20 14:41:22 +0300

    tests: basic end-to-end integration tests

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from urllib.error import URLError
+from urllib.request import urlopen
+
+import pytest
+from requests.exceptions import ConnectionError
+
+from reiz.config import sync_config
+
+PROJECT_PATH = Path(__file__).parent.parent
+STATIC_PATH = PROJECT_PATH / "static"
+
+
+def query(host, query_str, **query_kwargs):
+    query = {"query": query_str, **query_kwargs}
+    query_data = json.dumps(query).encode()
+    with urlopen(host, data=query_data) as page:
+        return json.load(page)
+
+
+def health_check(host):
+    try:
+        query(host, "Name()")
+    except URLError:
+        return False
+    else:
+        return True
+
+
+@pytest.fixture(scope="session")
+def reiz_service(docker_ip, docker_services):
+    docker_cfg = sync_config(STATIC_PATH / "configs" / "docker_config.json")
+    port = docker_services.port_for("reiz", docker_cfg.web.port)
+    url = "http://{}:{}".format(docker_ip, port)
+    docker_services.wait_until_responsive(
+        timeout=30.0, pause=0.1, check=lambda: health_check(url)
+    )
+    return url
+
+
+def test_reiz_web_api_basic(reiz_service):
+    assert query("Name()")
